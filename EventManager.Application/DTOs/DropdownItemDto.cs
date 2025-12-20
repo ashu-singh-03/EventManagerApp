62ab2a61e7af50f8957ed194f6504b90f61843fa
Ashutosh singh <ashutoshsinghsingh1256@gmail.com>
2025-12-20 19:17:14 +0530

    CHECK IN

---
+++ EventManager.Application/DTOs/DropdownItemDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventManager.Application.DTOs
+{
+    public class DropdownItemDto
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
